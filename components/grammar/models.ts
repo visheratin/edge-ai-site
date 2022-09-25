4afd53d9903a77ba3d453981c310a167a5fffd92
Alexander Visheratin <alexvish91@gmail.com>
2022-09-24 23:28:23 -0400

    Removed redundant model.

--- components/grammar/models.ts
+++ components/grammar/models.ts
@@ -2,20 +2,7 @@ import { Metadata } from "./metadata";
 
 export const grammarModels: Metadata[] = [
     {
-        title: "T5 small",
-        models: new Map<string, string>([
-            ["encoder", "/_next/static/chunks/pages/grammar/t5-small-encoder-quantized.onnx"],
-            ["decoder", "/_next/static/chunks/pages/grammar/t5-small-decoder-quantized.onnx"],
-            ["init-decoder", "/_next/static/chunks/pages/grammar/t5-small-init-decoder-quantized.onnx"],
-        ]),
-        tokenizer: "/_next/static/chunks/pages/grammar/tokenizer.json",
-        examples: [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus, enim viverra auctor tempus.",
-            "Morbi elit mi, accumsan vitae elementum tincidunt, feugiat sed risus."
-        ]
-    },
-    {
-        title: "Small model",
+        title: "T5 Efficient TINY",
         models: new Map<string, string>([
             ["encoder", "/_next/static/chunks/pages/grammar/encoder.onnx"],
             ["decoder", "/_next/static/chunks/pages/grammar/decoder.onnx"],
