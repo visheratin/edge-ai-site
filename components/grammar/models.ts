7ec385a97107bfbfdef9bf627294617aebc8ad2b
Alexander Visheratin <alexvish91@gmail.com>
2022-10-02 10:57:48 -0400

    Update models.

--- components/grammar/models.ts
+++ components/grammar/models.ts
@@ -4,9 +4,9 @@ export const grammarModels: Metadata[] = [
     {
         title: "T5 Efficient TINY",
         models: new Map<string, string>([
-            ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/encoder.onnx"],
-            ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/decoder.onnx"],
-            ["init-decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/decoder-init.onnx"],
+            ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-encoder.onnx"],
+            ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-decoder.onnx"],
+            ["init-decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-decoder-init.onnx"],
         ]),
         tokenizer: "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tokenizer.json",
         examples: []
@@ -14,13 +14,23 @@ export const grammarModels: Metadata[] = [
     {
         title: "T5 Efficient TINY quantinized",
         models: new Map<string, string>([
-            ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/encoder-quant.onnx"],
-            ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/decoder-quant.onnx"],
-            ["init-decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/decoder-init-quant.onnx"],
+            ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-encoder-quant.onnx"],
+            ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-decoder-quant.onnx"],
+            ["init-decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-decoder-init-quant.onnx"],
         ]),
         tokenizer: "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tokenizer.json",
         examples: []
     },
+    {
+        title: "T5 Efficient MINI",
+        models: new Map<string, string>([
+            ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/mini-encoder.onnx"],
+            ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/mini-decoder.onnx"],
+            ["init-decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/mini-decoder-init.onnx"],
+        ]),
+        tokenizer: "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tokenizer.json",
+        examples: []
+    }
     {
         title: "T5 Efficient MINI quantinized",
         models: new Map<string, string>([
