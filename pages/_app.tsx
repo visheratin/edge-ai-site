599559baf826819a4e2a97e2a81a0c2b612e66b4
Alexander Visheratin <alexvish91@gmail.com>
2022-10-02 01:15:52 -0400

    Added favicon.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,26 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
 import '@datadog/browser-logs/bundle/datadog-logs'
 import { useEffect } from 'react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    window.DD_LOGS.init({
-      clientToken: 'pub1df46ecb733718bfa44a66ec0e6a5723',
-      site: 'datadoghq.com',
-      forwardErrorsToLogs: true,
-      sampleRate: 100,
-    })
-    console.log("Initialized Datadog")
+    if (typeof window.DD_LOGS === "undefined") {
+      window.DD_LOGS.init({
+        clientToken: 'pub1df46ecb733718bfa44a66ec0e6a5723',
+        site: 'datadoghq.com',
+        forwardErrorsToLogs: true,
+        sampleRate: 100,
+      })
+      console.log("Initialized Datadog")
+    }
   })
 
   return (
     <Layout>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
