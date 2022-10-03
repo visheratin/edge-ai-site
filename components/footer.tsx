76f130a6604c4823e9e9f1e9e1e4e8aa0a6c0559
Alexander Visheratin <alexvish91@gmail.com>
2022-10-03 00:36:56 -0400

    Fixed footer.

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,18 +8,18 @@ export default function Footer() {
         <div className="divider"></div>
         <div className="container section">
           <div className="row">
-            <div className="col offset-l3 l1 s4 center-align">
+            <div className="col offset-l3 l1 s4 center-align hide-on-small-only">
             </div>
-            <div className="col l1 s4 center-align">
+            <div className="col l1 s3 center-align">
               <SocialIcon url="https://github.com/visheratin/edge-ai-site" target="_blank" />
             </div>
-            <div className="col l1 s4 center-align">
+            <div className="col l1 s3 center-align">
               <SocialIcon url="https://www.linkedin.com/in/visheratin/" target="_blank" />
             </div>
-            <div className="col l1 s4 center-align">
+            <div className="col l1 s3 center-align">
               <SocialIcon url="https://twitter.com/visheratin" target="_blank" />
             </div>
-            <div className="col l1 s4 center-align">
+            <div className="col l1 s3 center-align">
               <SocialIcon url="mailto:alex@visheratin.com" target="_blank" />
             </div>
           </div>
