9685de26377a0aeb321fd4081c7c74e33b33e4dd
Alexander Visheratin <alexvish91@gmail.com>
2022-10-04 12:53:09 -0400

    Removed nextjs links.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,24 +5,16 @@ export default function Navbar() {
     <>
       <nav className="green darken-1" role="navigation">
         <div className="nav-wrapper container">
-          <Link href="/" passHref={true}>
-            <a id="logo-container" className="brand-logo">Edge AI</a>
-          </Link>
+          <a href="/" id="logo-container" className="brand-logo">Edge AI</a>
           <ul className="right hide-on-med-and-down">
             <li>
-              <Link href="/models/sem-segment" passHref={true}>
-                <a>Microscopy images segmentation</a>
-              </Link>
+              <a href="/models/sem-segment">Microscopy images segmentation</a>
             </li>
             <li>
-              <Link href="/models/grammar-check" passHref={true}>
-                <a>Grammar check</a>
-              </Link>
+              <a href="/models/grammar-check">Grammar check</a>
             </li>
             <li>
-              <Link href="/models/segment" passHref={true}>
-                <a>Images segmentation</a>
-              </Link>
+              <a href="/models/segment">Images segmentation</a>
             </li>
           </ul>
         </div>
