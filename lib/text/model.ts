edb884c5c7611a315d0d1c6dc6e2ad0f57b9ff6b
Alexander Visheratin <alexvish91@gmail.com>
2022-11-07 23:04:22 -0500

    Fixed model logic.

--- lib/text/model.ts
+++ lib/text/model.ts
@@ -1,30 +1,13 @@
 import { createSession } from "../session";
 import Tokenizer from "./tokenizer";
 import { Metadata } from "./metadata";
-import { split } from "sentence-splitter";
 import T5ForConditionalGeneration from "./transformers";
 
-interface SentencePart {
-  type: string;
-  value: string;
-}
-
-export interface SentenceMetrics {
-  length: number;
+export type Seq2SeqResult = {
+  text: string;
+  cached: boolean;
   tokensNum: number;
   elapsed: number;
-}
-
-interface TextPartOutput {
-  text: string;
-  inputLength: number;
-  tokensLength: number;
-  elapsed: number;
-}
-
-export type Seq2SeqResult = {
-  result: string;
-  metrics: Array<SentenceMetrics>;
 };
 
 export class Seq2SeqModel {
@@ -64,56 +47,14 @@ export class Seq2SeqModel {
   };
 
   process = async (input: string): Promise<Seq2SeqResult> => {
-    let result: Seq2SeqResult = {
-      result: "",
-      metrics: new Array<SentenceMetrics>(),
-    };
-    let textParts = this.splitText(input);
-    let output = "";
-    for (let part of textParts) {
-      if (part.type === "Str") {
-        if (this.cache.has(part.value)) {
-          output = output.concat(this.cache.get(part.value) as string);
-        } else {
-          if (part.value.length < 2) {
-            output = output.concat(part.value);
-          } else {
-            const partOutput = await this.processTextPart(part.value);
-            output = output.concat(partOutput.text);
-            this.cache.set(part.value, partOutput.text);
-            result.metrics.push({
-              length: partOutput.inputLength,
-              tokensNum: partOutput.tokensLength,
-              elapsed: partOutput.elapsed,
-            });
-          }
-        }
-      } else {
-        if (!output.endsWith(part.value)) {
-          output = output.concat(part.value);
-        }
-      }
+    if (this.cache.has(input)) {
+      return {
+        text: this.cache.get(input) as string,
+        cached: true,
+        tokensNum: 0,
+        elapsed: 0,
+      };
     }
-    result.result = output;
-    return result;
-  };
-
-  splitText = (text: string): Array<SentencePart> => {
-    let parts = split(text);
-    let result: Array<SentencePart> = new Array<SentencePart>();
-    for (let part of parts) {
-      if (part.type === "Sentence") {
-        for (let childNode of part.children) {
-          result.push({ type: childNode.type, value: childNode.value });
-        }
-      } else {
-        result.push({ type: part.type, value: part.value });
-      }
-    }
-    return result;
-  };
-
-  processTextPart = async (input: string): Promise<TextPartOutput> => {
     const generationOptions = {
       maxLength: 500,
       topK: 0,
@@ -130,8 +71,8 @@ export class Seq2SeqModel {
     output = output.trim();
     return {
       text: output,
-      inputLength: input.length,
-      tokensLength: inputTokenIds?.length!,
+      cached: false,
+      tokensNum: inputTokenIds?.length!,
       elapsed: elapsed,
     };
   };
