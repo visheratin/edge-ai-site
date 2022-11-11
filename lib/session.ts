a63c645904bff5433b98d62fc0a67027cd750cae
Alexander Visheratin <alexvish91@gmail.com>
2022-11-11 08:32:07 -0500

    Added webgl as a default backend.

--- lib/session.ts
+++ lib/session.ts
@@ -12,10 +12,18 @@ export const createSession = async (
   modelPath: string
 ): Promise<ort.InferenceSession> => {
   const model_data = await fetch(modelPath).then((resp) => resp.arrayBuffer());
-  const session = await ort.InferenceSession.create(model_data, {
-    executionProviders: ["wasm"],
-    graphOptimizationLevel: "all",
-    executionMode: "parallel",
-  });
-  return session;
+  try {
+    const session = await ort.InferenceSession.create(model_data, {
+      executionProviders: ["webgl"],
+      graphOptimizationLevel: "all",
+    });
+    return session;
+  } catch {
+    const session = await ort.InferenceSession.create(model_data, {
+      executionProviders: ["wasm"],
+      graphOptimizationLevel: "all",
+      executionMode: "parallel",
+    });
+    return session;
+  }
 };
